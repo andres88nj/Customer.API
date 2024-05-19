dd8b821d3d070b8e2352d2f3d23345068e5ea8bd
andres88nj <andres88nj@gmail.com>
2024-05-19 00:20:45 -0500

    update

--- src/Customer.Infrastructure/AppDbContextSeed.cs
+++ src/Customer.Infrastructure/AppDbContextSeed.cs
@@ -23,7 +23,13 @@ public class AppDbContextSeed
     {
         return new List<Customers>
         {
-            new Customers {Name = "Jack Sparrow", DNI = "123456", Address = "Sea", Phone = "+15458", Mobile = "+15888", Email = "test@test.com", City = "city", State = "state", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, CreatedBy = "Admin", UpdatedBy = "Admin" }
+            new Customers { Name = "Peter Jhonson", DNI = "123456", Address = "Sea", Phone = "15458", Mobile = "15888", Email = "test1@test.com", City = "Miami", State = "Florida", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, CreatedBy = "Admin", UpdatedBy = "Admin" },
+            new Customers { Name = "Jhon Strauss", DNI = "345345", Address = "Test", Phone = "4534", Mobile = "87654", Email = "test2@test.com", City = "Orlando", State = "Florida", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, CreatedBy = "Admin", UpdatedBy = "Admin" },
+            new Customers { Name = "Tony Diaz", DNI = "567567", Address = "ABC", Phone = "7654", Mobile = "3456", Email = "test3@test.com", City = "California", State = "Los Angeles", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, CreatedBy = "Admin", UpdatedBy = "Admin" },
+            new Customers { Name = "Carl Jackson", DNI = "24356", Address = "Home", Phone = "21231", Mobile = "23856", Email = "test4@test.com", City = "California", State = "Chicago", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, CreatedBy = "Admin", UpdatedBy = "Admin" },
+            new Customers { Name = "Jack Obama", DNI = "9876", Address = "Test 3", Phone = "547567", Mobile = "9765432", Email = "test5@test.com", City = "California", State = "San Francisco", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, CreatedBy = "Admin", UpdatedBy = "Admin" },
+            new Customers { Name = "Homer Asus", DNI = "645345", Address = "Test 4", Phone = "23435", Mobile = "32856", Email = "test6@test.com", City = "New York", State = "New York", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now, CreatedBy = "Admin", UpdatedBy = "Admin" },
+
         };
 
     }
