8ed368b39fadba72d96fd88bdc064d9b602b4a63
andres88nj <andres88nj@gmail.com>
2024-05-19 19:39:14 -0500

    AddValidationTesting

---
+++ src/Customer.API/Customer.API.UnitTests/Validations/CreateCustomerCommandValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using Customer.Domain.DTO;
+using Customer.Domain.Validations;
+using Customer.Domain.Validations.Customer;
+using FluentValidation.TestHelper;
+
+namespace Customer.API.UnitTests.Validations;
+
+public class CreateCustomerCommandValidatorTests
+{
+    private readonly CreateCustomerCommandValidator _validator;
+
+    public CreateCustomerCommandValidatorTests()
+    {
+        _validator = new CreateCustomerCommandValidator();
+    }
+
+    [Fact]
+    public void CreateCustomerCommandValidator_ShouldHaveError()
+    {
+        var request = new CreateCustomerRequest();
+
+        var result = _validator.TestValidate(request);
+
+        result.ShouldHaveValidationErrorFor(request => request.Name).WithErrorMessage(ValidationErrors.ECC001.Message);
+    }
+}
